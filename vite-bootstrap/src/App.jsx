bf87d0a526ef88ad124624a8003374460127ed9b
Pedro L Briceno <pedrobrice91@gmail.com>
2025-07-04 14:47:24 -0400

    primero

--- vite-bootstrap/src/App.jsx
+++ vite-bootstrap/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Card />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
