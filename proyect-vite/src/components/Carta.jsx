46143df90b5750fdfadb11ba05b60c2da70bc808
Pedro L Briceno <pedrobrice91@gmail.com>
2025-07-06 20:26:36 -0400

    props and useState

--- proyect-vite/src/components/Carta.jsx
+++ proyect-vite/src/components/Carta.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 function Carta(props) {
+  let [contador, setContador] = useState(0);
+
   return (
     <div>
       <div className="card" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={props.imgUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.text}</p>
-          <a href="#" className="btn btn-primary">
+          <p>{contador}</p>
+          <a
+            href="#"
+            className="btn btn-primary text-light"
+            onClick={() => {
+              setContador(contador + 1);
+
+              console.log(`Has pulsado ${contador} veces`);
+            }}
+          >
             Go somewhere
           </a>
         </div>
