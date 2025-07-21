79e40fd3d506ccbc776b793dce5897511f141174
Pedro L Briceno <pedrobrice91@gmail.com>
2025-07-21 19:35:16 -0400

    21

---
+++ proyect-vite/src/Apps.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home.jsx";
+function Apps() {
+    return (
+    <div>
+      <div className="container-fluid">
+      <div class="row align-items-start">
+        <h1>💰 Calculadora de Propinas</h1>
+        <h2>Calcula fácilmente tu propina y total</h2>
+       <Home/>
+      </div>
+      </div>
+    </div>
+    );
+  }
+  
+  export default Apps;
+  
