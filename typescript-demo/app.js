edc5be3a8d9f66d7717c1b44f3e0f8c904d58af7
Pedro L Briceno <pedrobrice91@gmail.com>
2025-08-20 19:32:46 -0400

    reestructurando

---
+++ typescript-demo/app.js
@@ -0,0 +1,4 @@
+function greet(name) {
+    return "Hello, ".concat(name, "! Welcome to TypeScript.");
+}
+console.log(greet("Developer"));
---
+++ typescript-demo/app.ts
@@ -0,0 +1,10 @@
+let numero = 2;
+numero = 3
+const firstName = 'pedro';
+const lastName = 'Luis'
+
+function greet(name: string): string {
+  return `Hello, ${name}! Welcome to TypeScript.`;
+}
+
+console.log(greet("Developer"));
