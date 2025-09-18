db8ea0ca255023e41bea0cf28773c8b595f8b136
Pedro L Briceno <pedrobrice91@gmail.com>
2025-09-18 12:31:06 -0300

    capitulos 1 al 7

---
+++ Ecommerce/src/Components/Navbar/index.jsx
@@ -0,0 +1,67 @@
+import { NavLink } from "react-router-dom";
+export default function Navbar() {
+  const activeStyle = 'underline underline-offset-4'
+  return (
+    <nav className="flex justify-between items-center text-sm m-8">
+      <ul className="flex items-center space-x-4">
+        <li className="font-semibold text-lg space-x-4">
+          <NavLink to="/shopi">Shopi</NavLink>
+        </li>
+        <li>
+          <NavLink to="/all"
+          className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>All</NavLink>
+        </li>
+        <li>
+          <NavLink to="/clothes" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>Clothes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/electronics" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>Electronics</NavLink>
+        </li>
+        <li>
+          <NavLink to="/furnitures" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>Furnitures</NavLink>
+        </li>
+        <li>
+          <NavLink to="/toys" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>Toys</NavLink>
+        </li>
+        <li>
+          <NavLink to="/others" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>Others</NavLink>
+        </li>
+      </ul>
+      <ul className="flex items-center space-x-4">
+        <li className="text-black/60">teff@platzi.com</li>
+        <li>
+          <NavLink to="/my-orders" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>My Orders</NavLink>
+        </li>
+        <li>
+          <NavLink to="/my-account" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>My Account</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sign-in" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>Sign In</NavLink>
+        </li>
+        <li>
+          <NavLink to="my-order" className={({ isActive}) =>
+             isActive ? activeStyle : undefined
+          }>🛒 0</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- Ecommerce/src/Pages/App/index.jsx
+++ Ecommerce/src/Pages/App/index.jsx
@@ -1,23 +1,27 @@
-import './App.css'
-import Home from '../Home'
-import MyAccount from '../MyAccount'
-import MyOrder from '../MyOrder'
-import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
-import SignIn from '../SignIn'
+import "./App.css";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from '../../Components/Navbar'
 
-export default function App(){
-return <>
-    <div>
-      <h1 className='text-3xl font-bold underline text-amber-600'>Vite + React</h1>
-    </div>  
-     
-    <Home/> 
-    <MyAccount/>
-    <MyOrder/>
-    <MyOrders/>
-    <NotFound/>
-    <SignIn/>
+export default function App() {
+  return (
+    <> 
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-account" element={<MyAccount />} />
+          <Route path="/my-order" element={<MyOrder />} />
+          <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-
+  );
 }
